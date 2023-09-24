5bf85a5d82b2dd6be63436f3841ef17280cef2a8
Ahmed Hashi <ahmedhashi1263@gmail.com>
2023-09-24 01:38:49 -0400

    Finished functions in assembly for MS1 and implemented main function

--- a2/csf_assign02/c_wcmain.c
+++ a2/csf_assign02/c_wcmain.c
@@ -15,16 +15,52 @@ int main(int argc, char **argv) {
   uint32_t best_word_count = 0;
 
   // TODO: implement
-  char*[] filename = argv[1];
-  FILE *inputFile = fopen
-  while (wc_readnext(FILE *in, unsigned char *w))
-  
+  FILE *file = fopen(argv[1], "r");
+
+  // Array representing the hash table (each element is like head of linked list)
+  struct WordEntry *buckets[HASHTABLE_SIZE] = {NULL};
+  unsigned char word[MAX_WORDLEN + 1];
+
+  while (wc_readnext(file, word)) {
+    total_words++;
+    wc_tolower(word);
+    wc_trim_non_alpha(word);
+
+    struct WordEntry *entry = wc_dict_find_or_insert(buckets, HASHTABLE_SIZE, word);
+    entry->count++;
+
+    if (entry->count > best_word_count) {
+      best_word_count = entry->count;
+      best_word = entry->word;
+    } else if (entry->count == best_word_count) {
+      if ((wc_str_compare(entry->word, best_word)) < 0) {
+        best_word = entry->word;
+      }
+    }
+  }
+
+  for (int i = 0; i < HASHTABLE_SIZE; i++) {
+    struct WordEntry *curr = buckets[i];
+
+    // linked list traversal
+    while (curr) {
+      unique_words++;
+      curr = curr->next;
+    }
+  }
+
 
   printf("Total words read: %u\n", (unsigned int) total_words);
   printf("Unique words read: %u\n", (unsigned int) unique_words);
   printf("Most frequent word: %s (%u)\n", (const char *) best_word, best_word_count);
 
   // TODO: make sure file is closed (if one was opened)
+  fclose(file);
+
+  for (int i = 0; i < HASHTABLE_SIZE; i++) {
+    wc_free_chain(buckets[i]);
+  }
+
   // TODO: make sure memory is freed
 
   return 0;
