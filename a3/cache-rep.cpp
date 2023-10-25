259919714d5f23a7ba87230776a30d39290b3a8c
Ahmed Hashi <ahmedhashi1263@gmail.com>
2023-10-25 13:25:02 -0400

    Added comments and fixed some Makefile things

--- a3/cache-rep.cpp
+++ a3/cache-rep.cpp
@@ -25,16 +25,16 @@ Cache::Cache(int slotSize, int numSlots, int numSets, bool writeThru, bool write
 }
 
 Cache::Set::Set(int numSlots) {
-    this->numInserts = 0;
-    this->max_ts = 0;
+    // this->numInserts = 0;
+    // this->max_ts = 0;
     for (int i = 0; i < numSlots; i++) {
         Slot slot;
         slot.valid = false;
         slot.dirty = false;
         slots.push_back(slot);
     }
-    // this->numInserts = 0;
-    // this->max_ts = 0;
+    this->numInserts = 0;
+    this->max_ts = 0;
     this->numSlots= numSlots;
 }
 
@@ -43,14 +43,15 @@ void Cache::getValue(uint32_t &tag, uint32_t &index, std::string memAddress) {
     uint32_t initialMem = std::stoul(memAddress,nullptr,16);
     // chop off offset bits
     initialMem = initialMem >> offsetBits;
-    uint32_t mask = (1 << indexBits) - 1;
-    index = initialMem & mask;
+    uint32_t indexFilter = (1 << indexBits) - 1;
+    index = initialMem & indexFilter;
     tag = initialMem >> indexBits;
 }
 
 int Cache::Set::evictLRU(uint32_t tag) {
     int lruSlot = 0;
     int curMinimum = max_ts;
+    // by the end of the for loop we will have the least recently used slot
     for (int i = 0; i < numSlots; i++) {
         if (slots[i].access_ts < curMinimum) {
             curMinimum = slots[i].access_ts;
@@ -66,6 +67,7 @@ int Cache::Set::evictLRU(uint32_t tag) {
 int Cache::Set::evictFIFO(uint32_t tag) {
     int lruSlot = 0;
     int firstEntry = numInserts;
+    // by the end of the for loop we will have the first insertion
     for (int i = 0; i < numSlots; i++) {
         if (slots[i].insert_ts < firstEntry) {
             firstEntry = slots[i].insert_ts;
@@ -79,12 +81,12 @@ int Cache::Set::evictFIFO(uint32_t tag) {
     return lruSlot;
 }
 
-bool Cache::Set::isHit(uint32_t tag, int &hitIndex) {
+bool Cache::Set::isHit(uint32_t tag, int &hitLocation) {
     for (int i = 0; i < numSlots; i++) {
         if (slots[i].tag == tag && slots[i].valid) {
             slots[i].access_ts = max_ts;
             max_ts++;
-            hitIndex = i;
+            hitLocation = i;
             return true;
         }
     }
@@ -110,28 +112,38 @@ void Cache::memStore(std::string memAddress) {
     numStores++;
     uint32_t index, tag;
     getValue(tag, index, memAddress);
-    int hitIndex = 0;
+    int hitLocation = 0;
     Set &curSet = sets[index];
-    if (curSet.isHit(tag, hitIndex)) {
+
+    // In the event of a hit
+    if (curSet.isHit(tag, hitLocation)) {
         storeHits++;
         numCycles++;
 
+        // WRITE-THROUGH
         if (writeThru) {
             numCycles += 100;
+
+        // WRITE-BACK
         } else {
-            curSet.slots[hitIndex].dirty = true;
+            curSet.slots[hitLocation].dirty = true;
         }
+
+    // In the event of a miss
     } else {
         storeMisses++;
+        // WRITE-ALLOCATE
         if (writeAlloc) {
             numCycles += 100 * (slotSize / 4);
             numCycles++;
+            // If it is full that means we have to evict by some method (either FIFO or LRU)
             if (curSet.isFull(tag)) {
                 int evictIndex =  evictMethod ? curSet.evictLRU(tag) : curSet.evictFIFO(tag); // curSet.evictLRU(tag); // CHANGE LATER
                 if (curSet.slots[evictIndex].dirty) {
                     numCycles += 100 * (slotSize / 4);
                 }
             }
+        // NO-WRITE-ALLOCATE
         } else {
             numCycles += 100;
         }
@@ -142,15 +154,16 @@ void Cache::memLoad(std::string memAddress) {
     uint32_t index, tag;
     numLoads++;
     getValue(tag, index, memAddress);
-    int hitIndex = 0;
+    int hitLocation = 0;
     Set &curSet = sets[index];
-    if (curSet.isHit(tag, hitIndex)) {
+    if (curSet.isHit(tag, hitLocation)) {
         numCycles++;
         loadHits++;
     } else {
         numCycles += 100 * (slotSize / 4);
         numCycles++;
         loadMisses++;
+        // If it is full that means we have to evict by some method (either FIFO or LRU)
         if (curSet.isFull(tag)) {
             evictMethod ? curSet.evictLRU(tag) : curSet.evictFIFO(tag);
             //evictMethod = curSet.evictFIFO(tag);
