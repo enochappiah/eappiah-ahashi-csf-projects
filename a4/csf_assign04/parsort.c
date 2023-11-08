53559d94f34426154fb46b61396d1a1aa9b1eb5c
Ahmed Hashi <ahmedhashi1263@gmail.com>
2023-11-08 00:14:40 -0500

    Restored functionality. Missing exit in left half

--- a4/csf_assign04/parsort.c
+++ a4/csf_assign04/parsort.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -60,19 +61,66 @@ void fatal(const char *msg) {
 void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
   assert(end >= begin);
   size_t size = end - begin;
+  size_t mid = begin + size / 2;
 
   if (size <= threshold) {
     seq_sort(arr, begin, end);
-    return;
-  }
 
-  // recursively sort halves in parallel
+  } else {
+    pid_t leftHalf = fork();
+    if (leftHalf == -1) {
+      fprintf(stderr, "Error: Left half fork failed to start child process\n");
+      exit(1);
+    } else if (leftHalf == 0) {
+      merge_sort(arr, begin, mid, threshold);
+      exit(0);
+    }
 
-  size_t mid = begin + size/2;
+    pid_t rightHalf = fork();
+    if (rightHalf == -1) {
+      fprintf(stderr, "Error: Right half fork failed to start child process\n");
+      exit(1);
+    } else if (rightHalf == 0) {
+      merge_sort(arr, mid, end, threshold);
+      exit(0);
+    }
 
-  // TODO: parallelize the recursive sorting
-  merge_sort(arr, begin, mid, threshold);
-  merge_sort(arr, mid, end, threshold);
+    int leftChildInfo;
+    int rightChildInfo;
+    pid_t leftWaitReturn = waitpid(leftHalf, &leftChildInfo, 0);
+    pid_t rightWaitReturn = waitpid(rightHalf, &rightChildInfo, 0);
+    if (leftWaitReturn == -1) {
+      fprintf(stderr, "Error: Something went wrong with child process\n");
+      exit(1);
+    }
+
+    if (!WIFEXITED(leftChildInfo)) {
+      fprintf(stderr, "Error: Left child did not exit normally\n");
+      exit(1);
+    }
+
+    if(WEXITSTATUS(leftChildInfo) != 0) {
+      fprintf(stderr, "Error: Bad exit status with left child\n");
+      exit(1);
+    }
+
+    if (rightWaitReturn == -1) {
+      fprintf(stderr, "Error: Something went wrong with child process\n");
+      exit(1);
+    }
+
+    if (!WIFEXITED(rightChildInfo)) {
+      fprintf(stderr, "Error: Right child did not exit normally\n");
+      exit(1);
+    }
+
+    if(WEXITSTATUS(rightChildInfo) != 0) {
+      fprintf(stderr, "Error: Bad exit status with right child\n");
+      exit(1);
+    }
+  
+
+
 
   // allocate temp array now, so we can avoid unnecessary work
   // if the malloc fails
@@ -92,6 +140,8 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
   free(temp_arr);
 
   // success!
+
+  }
 }
 
 int main(int argc, char **argv) {
@@ -107,17 +157,53 @@ int main(int argc, char **argv) {
   size_t threshold = (size_t) strtoul(argv[2], &end, 10);
   if (end != argv[2] + strlen(argv[2])) {
     // TODO: report an error (threshold value is invalid)
+    fprintf(stderr, "Error: Threshold value is invalid\n");
+    return 1;
   }
 
   // TODO: open the file
+  int fd = open(filename, O_RDWR);
+  if (fd < 0) {
+    fprintf(stderr, "Error: Could not open file\n");
+    return 1;
+  }
 
   // TODO: use fstat to determine the size of the file
+  struct stat statbuf;
+  int rc = fstat(fd, &statbuf);
+  if (rc != 0) {
+    fprintf(stderr, "Error: Could not determine size of file\n");
+    close(fd);
+    return 1;
+  }
+
+  size_t file_size_in_bytes = statbuf.st_size;
+
 
   // TODO: map the file into memory using mmap
+  int64_t *data = mmap(NULL, file_size_in_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (data == MAP_FAILED) {
+    fprintf(stderr, "Error: Could not map to memory\n");
+    close(fd);
+    return 1;
+  }
 
   // TODO: sort the data!
+  size_t len = file_size_in_bytes/ sizeof(int64_t);
+  merge_sort(data, 0, len, threshold);
 
   // TODO: unmap and close the file
+  if (munmap(data, file_size_in_bytes) == -1) {
+    fprintf(stderr, "Error: Unmapping did not work properly\n");
+    close(fd);
+    return 1;
+  }
+
+  if (close(fd) == -1) {
+    fprintf(stderr, "Error: Closing file produced error\n");
+    return 1;
+  }
+  return 0;
 
   // TODO: exit with a 0 exit code if sort was successful
 }
