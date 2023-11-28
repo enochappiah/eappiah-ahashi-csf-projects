1534d0fbd7c693df6157623f6d27c2cd10e07070
Ahmed Hashi <ahmedhashi1263@gmail.com>
2023-11-27 22:32:05 -0500

    Raw implementation of connection and sender

--- a5/connection.cpp
+++ a5/connection.cpp
@@ -1,66 +1,114 @@
-#include <iostream>
 #include <sstream>
 #include <cctype>
 #include <cassert>
+#include <stdio.h>
+#include <string>
 #include "csapp.h"
 #include "message.h"
 #include "connection.h"
 
+
 Connection::Connection()
-  : m_fd(-1)
-  , m_last_result(SUCCESS) {
+ : m_fd(-1)
+ , m_last_result(SUCCESS) {
 }
+
 
 Connection::Connection(int fd)
-  : m_fd(fd)
-  , m_last_result(SUCCESS) {
-  // TODO: call rio_readinitb to initialize the rio_t object
-  rio_t rio;
-  rio_readinitb(&rio, fd);
+ : m_fd(fd)
+ , m_last_result(SUCCESS) {
+ // TODO: call rio_readinitb to initialize the rio_t object
+ rio_readinitb(&m_fdbuf, m_fd);
 }
+
 
 void Connection::connect(const std::string &hostname, int port) {
-  // TODO: call open_clientfd to connect to the server
-  // TODO: call rio_readinitb to initialize the rio_t object
-  int fd = open_clientfd(hostname.c_str(), port);
-  if (fd < 0) {
-    std::cerr << "Couldn't connect to server\n";
-  }
+ // TODO: call open_clientfd to connect to the server
+ // TODO: call rio_readinitb to initialize the rio_t object
+ std::string port_string = std::to_string(port);
+
+
+ int fd = open_clientfd(hostname.c_str(), port_string.c_str());
+
+
+ if (fd < 0) {
+   // FILL IN
+ }
+
+
+ rio_readinitb(&m_fdbuf, m_fd);
 }
+
 
 Connection::~Connection() {
-  // TODO: close the socket if it is open
+ // TODO: close the socket if it is open
+ if (is_open()) {
+   Close(m_fd);
+ }
 }
+
 
 bool Connection::is_open() const {
-  // TODO: return true if the connection is open
+ // TODO: return true if the connection is open
+ return m_fd >= 0;
 }
+
 
 void Connection::close() {
-  // TODO: close the connection if it is open
+ // TODO: close the connection if it is open
+ if (is_open()) {
+   Close(m_fd);
+   // alter file descriptor
+ }
 }
+
 
 bool Connection::send(const Message &msg) {
-  // TODO: send a message
-  // return true if successful, false if not
-  // make sure that m_last_result is set appropriately
+ // TODO: send a message
+ // return true if successful, false if not
+ // make sure that m_last_result is set appropriately
+ std::string message = msg.tag + ":" + msg.data + "/n";
 
 
-  rio_writen(fd, msg, sizeof(msg));
-  rio_writen(fd, "\n", 1);
+ if (message.length() > Message::MAX_LEN || msg.tag != TAG_ERR || msg.tag != TAG_OK || msg.tag != TAG_SLOGIN || msg.tag != TAG_RLOGIN || msg.tag != TAG_JOIN || msg.tag != TAG_LEAVE || msg.tag != TAG_SENDALL || msg.tag != TAG_SENDUSER || msg.tag != TAG_QUIT || msg.tag != TAG_DELIVERY || msg.tag != TAG_EMPTY) {
+   m_last_result = INVALID_MSG;
+   return false;
+ }
 
 
-
-  
+ Rio_writen(m_fd, message.c_str(), message.length()); // use a try-catch??
+ m_last_result = SUCCESS;
+ return true;
 }
 
-bool Connection::receive(Message &msg) {
-  // TODO: receive a message, storing its tag and data in msg
-  // return true if successful, false if not
-  // make sure that m_last_result is set appropriately
 
-  rio_t rio;
-  rio_readinitb(&rio, fd);
-  char buf[1000];
-  ssize_t n = rio_readlineb(&rio, buf, sizeof(buf));
+bool Connection::receive(Message &msg) {
+ // TODO: receive a message, storing its tag and data in msg
+ // return true if successful, false if not
+ // make sure that m_last_result is set appropriately
+
+
+ char buf[Message::MAX_LEN];
+
+
+ ssize_t num_bytes = Rio_readlineb(&m_fdbuf, buf, Message::MAX_LEN);
+
+
+ if (num_bytes <= 0) {
+   m_last_result = EOF_OR_ERROR; // invalid_msg?
+   return false;
+ }
+
+
+ // null terminator?
+
+
+ std::string received_message(buf);
+ size_t colon_index = received_message.find(":");
+ msg.tag = received_message.substr(0, colon_index);
+ msg.data = received_message.substr(colon_index + 1);
+
+
+ m_last_result = SUCCESS;
+ return true;
 }
--- a5/sender.cpp
+++ a5/sender.cpp
@@ -1,104 +1,3 @@
-#include <cstdio>
-#include <cstring>
-#include <exception>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <stdexcept>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include "csapp.h"
-#include "message.h"
-#include "connection.h"
-#include "client_util.h"
-#include "cstring"
-
-int main(int argc, char **argv) {
-  if (argc != 4) {
-    std::cerr << "Usage: ./sender [server_address] [port] [username]\n";
-    return 1;
-  }
-
-  std::string server_hostname;
-  int server_port;
-  std::string username;
-
-  server_hostname = argv[1];
-  server_port = std::stoi(argv[2]);
-  username = argv[3];
-
-  // TODO: connect to server
-  Connection connection;
-  try {
-  connection.connect(server_hostname, server_port);
-  } catch (const std::exception &e) {
-    //TODO fill later
-  }
-
-  // TODO: send slogin message
-  Message sloginMessage (TAG_SLOGIN, username);
-  
-
-
-  // TODO: loop reading commands from user, sending messages to
-  //       server as appropriate
-
- 
-
-  
-
-  
-
-
-
-
-  // rio_t rio;
-  // rio_readinitb(&rio, fd);
-  // char buf[1000];
-  // ssize_t n = rio_readlineb(&rio, buf, sizeof(buf));
-  // if (n < 0) {
-  //   return 1; // error reading data from user
-  // }
-  
-
-  //rio_t rio;
-  //rio_readinitb(&rio, fd);
-
-  // while (true) {
-  //   std::string input;
-  //   //std::cout << "> ";
-  //   // std::getline(std::cin, input);
-
-  //   // if (input.empty()) {
-  //   //   continue; // Skip empty input
-  //   // }
-  //   ssize_t n = rio_readlineb(&rio, buf, sizeof(buf));
-  //   if (n < 0) {
-  //   return 1; // error reading data from user
-  //   }
-
-  //   // TODO: Process the user command and send the appropriate message to the server
-  //   if (strcmp(buf, const char *s2))
-  //   if (buf.substr(0, 5) == "join:") {
-  //     // Send join message
-  //     rio_writen(fd, input.c_str(), input.length());
-  //     rio_writen(fd, "\n", 1);
-  //   } else if (input.substr(0, 7) == "slogout") {
-  //     // Send slogout message and exit the loop
-  //     rio_writen(fd, "slogout\n", 8);
-  //     break;
-  //   } else {
-  //     // Assume the input is a regular message
-  //     // Send the message to the server
-  //     rio_writen(fd, input.c_str(), input.length());
-  //     rio_writen(fd, "\n", 1);
-  //   }
-  // }
-
-  close(fd); // Close the connection
-
-  return 0;
-}
 #include <exception>
 #include <iostream>
 #include <string>
@@ -109,86 +8,107 @@ int main(int argc, char **argv) {
 #include "connection.h"
 #include "client_util.h"
 
+
 int main(int argc, char **argv) {
-  if (argc != 4) {
-    std::cerr << "Usage: ./sender [server_address] [port] [username]\n";
-    return 1;
-  }
+ if (argc != 4) {
+   std::cerr << "Usage: ./sender [server_address] [port] [username]\n";
+   return 1;
+ }
 
-  std::string server_hostname;
-  int server_port;
-  std::string username;
 
-  server_hostname = argv[1];
-  server_port = std::stoi(argv[2]);
-  username = argv[3];
+ std::string server_hostname;
+ int server_port;
+ std::string username;
 
-  // TODO: connect to server
 
-  Connection connection;
-  try {
-    connection.connect(server_hostname, server_port);
-  } catch (const std::exception &e) {
-    // Fill in
-  }
-  
+ server_hostname = argv[1];
+ server_port = std::stoi(argv[2]);
+ username = argv[3];
+
+
+ // TODO: connect to server
+
+
+ Connection connection;
+ try {
+   connection.connect(server_hostname, server_port);
+ } catch (const std::exception &e) {
+   // Fill in
+ }
   Message slogin_message(TAG_SLOGIN, username);
 
-  try {
-    connection.send(slogin_message);
-  } catch (const std::exception &e) {
-    // Fill in
-  }
 
-  std::string input;
-  while (true) {
-    if (!std::getline(std::cin, input)) {
-      break;
-    }
-
-    input = trim(input);
-
-    if (input == "/quit") {
-      Message user_quit(TAG_QUIT, "");
-      try {
-        connection.send(user_quit);
-      } catch (const std::exception &e) {
-        // Fill in
-      }
-      break;
-    }
-
-    if (input[0] == '/') {
-
-      // Handle join and leave
-
-    } else {
-      // normal message
-      Message send_to_everyone(TAG_SENDALL, input);
-      try {
-        connection.send(send_to_everyone);
-      } catch (const std::exception &e) {
-        // Fill in
-      }
-    }
+ try {
+   connection.send(slogin_message);
+ } catch (const std::exception &e) {
+   // Fill in
+ }
 
 
+ std::string input;
+ while (true) {
+   if (!std::getline(std::cin, input)) {
+     break;
+   }
 
-  }
+
+   input = trim(input);
+
+
+   if (input == "/quit") {
+     Message user_quit(TAG_QUIT, "");
+     try {
+       connection.send(user_quit);
+     } catch (const std::exception &e) {
+       // Fill in
+     }
+     break;
+   }
+
+
+   if (input[0] == '/') {
+
+
+     // Handle join and leave
+
+
+   } else {
+     // normal message
+     Message send_to_everyone(TAG_SENDALL, input);
+     try {
+       connection.send(send_to_everyone);
+     } catch (const std::exception &e) {
+       // Fill in
+     }
+   }
 
 
 
 
-  // TODO: send slogin message
 
-  // TODO: loop reading commands from user, sending messages to
-  //       server as appropriate
 
-  try {
-    connection.close();
-  } catch (const std::exception &e) {
-    // Fill in
-  }
+ }
 
-  return 0;
+
+
+
+
+
+
+
+ // TODO: send slogin message
+
+
+ // TODO: loop reading commands from user, sending messages to
+ //       server as appropriate
+
+
+ try {
+   connection.close();
+ } catch (const std::exception &e) {
+   // Fill in
+ }
+
+
+ return 0;
 }
